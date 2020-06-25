f05e0746571ccac5aa80dfad3a9b0a10f63e53d4
gsvarma <17321286+gsvarma@users.noreply.github.com>
2020-06-25 22:20:40 +0200

    Adds Hyd Jubilee Hills Center location

---
+++ routes/locations.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+// Location 1
+let obj = {};
+obj.name = "Hyderabad Jubilee Hills Center"
+obj.latitude = 17.468069;
+obj.longitude  = 78.543648;
+let jsonString = JSON.stringify(obj);
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(jsonString);
+});
+
+module.exports = router;
