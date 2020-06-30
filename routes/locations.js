8f58c137d698ce6c3d83ffdbf76589312ca7f4fc
gsvarma <17321286+gsvarma@users.noreply.github.com>
2020-06-30 19:27:29 +0200

    Fixes JSON response

--- routes/locations.js
+++ routes/locations.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-// Location 1
-let obj = {};
-obj.name = "Hyderabad Jubilee Hills Center"
-obj.latitude = 17.468069;
-obj.longitude  = 78.543648;
-let jsonString = JSON.stringify(obj);
+// // Location 1
+// let obj = {};
+// obj.name = "Hyderabad Jubilee Hills Center"
+// obj.latitude = 17.468069;
+// obj.longitude  = 78.543648;
+// let jsonString = JSON.stringify(obj);
+
+const locationData = [{id: 1,name: 'Hyderabad Jubilee Hills Center',latitude: 17.468069, longitude: 78.543648}];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(jsonString);
+  res.json(locationData);
 });
 
 module.exports = router;
